c06fe389bca2814401919c5b715f5fd94503a5ad
Talia <139439068+Honk-423@users.noreply.github.com>
2025-06-02 16:31:09 -0500

    Create another variable and add it to string
    Create a new variable called finalString and assign it the value
    string. Concatenate finalString to string using the += operater.
    Return finalString outside of the loop. This adds the value of string
    to finalString upon every loop and returns it afterwards.

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,8 +1,16 @@
 const repeatString = function(string, num) {
+    
+let finalString = string;
 
-for (i = 1; i <= num; i++);
+for (i = 1; i <= num; i++) {
+    finalString += string;
+}
+
+return finalString;
 
 };
+
+// repeatString("hey", 3);
 
 // Do not edit below this line
 module.exports = repeatString;
