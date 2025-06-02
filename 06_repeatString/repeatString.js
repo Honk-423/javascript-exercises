7bab9c130e4e39c84c0562da63db31042ee65115
Talia <139439068+Honk-423@users.noreply.github.com>
2025-06-02 17:00:25 -0500

    Make repeatString return error if num is negative
    Use an else if statement to do this. Remove corresponding skip to pass
    test.

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -8,6 +8,8 @@ for (i = 1; i < num; i++) {
 
 if (num > 0) {
     return finalString;
+} else if (num < 0) {
+    return "ERROR";
 } else {
     return "";
 }
@@ -21,6 +23,8 @@ repeatString("hello", 10);
 repeatString("hi", 1);
 
 repeatString("bye", 0);
+
+repeatString("goodbye", -1);
 
 // Do not edit below this line
 module.exports = repeatString;
