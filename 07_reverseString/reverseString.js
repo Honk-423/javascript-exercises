25ee5f5392273601e74d0dd73f4fc08d96903a58
Talia <139439068+Honk-423@users.noreply.github.com>
2025-08-19 15:30:53 -0500

    docs: write pseudocode for reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,18 @@
 const reverseString = function(string) {
 
+const array = [...string];
+// SPLIT string
+// REVERSE string
+// JOIN string
+// RETURN string
+
 };
+
+// "Strings in JavaScript cannot be reversed directly so you're going 
+// to have to split it into something else first.." An array?
+// Try splitting the string into an array then 
+// rearranging it backwards.
+// Make the array output every letter starting from the last one.
 
 // Do not edit below this line
 module.exports = reverseString;
