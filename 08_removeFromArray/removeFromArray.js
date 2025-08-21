b96678c43ba64985035bfb17a1e4973c237c4ae0
Talia <139439068+Honk-423@users.noreply.github.com>
2025-08-21 15:07:51 -0500

    docs: add pseudocode to removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,11 +1,9 @@
 const removeFromArray = function() {
+    // ADD params arr and elem to function
+    // SPLICE arr from elem with delete count of one
+    // RETURN arr
 };
 
-// ADD params arr and elem to function
-// FINDindex of elem
-// REASSIGN elem to indexOf elem
-// SPLICE arr from elem with delete count of one
-// RETURN arr
 
 // Do not edit below this line
 module.exports = removeFromArray;
