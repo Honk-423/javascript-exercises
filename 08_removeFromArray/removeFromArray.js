6197ade62f7cbcb551ae1e0a3ccd9cece982932c
Talia <139439068+Honk-423@users.noreply.github.com>
2025-08-21 15:53:15 -0500

    feat: make func able remove multiple of same value
    Make removeFromArray function able to remove mutliple matching values

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -4,13 +4,16 @@ const removeFromArray = function(arr, ...elems) {
         // INIT for... of loop
         arr.splice(arr.indexOf(elem), 1);
         // SPLICE arr from elem with delete count of one 
+        if (arr[elem]) {
+            arr.splice(arr.indexOf(elem), 1);
+        }
     }
     console.log(arr);
     return arr;
     // RETURN arr
 };
 
-removeFromArray([1, 2, 3, 4], 3, 2);
+removeFromArray([1, 2, 2, 3, 4], 2);
 
 
 // Do not edit below this line
