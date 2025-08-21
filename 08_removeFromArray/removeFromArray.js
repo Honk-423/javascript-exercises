1ab94fc48a0a9d5db12574e396d5d813841f0604
Talia <139439068+Honk-423@users.noreply.github.com>
2025-08-21 15:09:38 -0500

    feat: make function remove single value from array

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,8 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, elem) {
     // ADD params arr and elem to function
+    arr.splice(arr.indexOf(elem), 1); // arr.indexOf(elem) works for a single value but that's it
     // SPLICE arr from elem with delete count of one
+    console.log(arr);
+    return arr;
     // RETURN arr
 };
+
+removeFromArray([1, 2, 3, 4], 3);
 
 
 // Do not edit below this line
