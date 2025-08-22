a657f49c50cab7004464f6c0d7a62932dd9569c5
Talia <139439068+Honk-423@users.noreply.github.com>
2025-08-22 15:30:42 -0500

    docs: add pseudocode

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -3,8 +3,11 @@ const removeFromArray = function(arr, ...elems) {
     for (let elem of elems) {
         // INIT for... of loop
         for (let i = 0; i < arr.length; i++) {
+            // INIT for loop
             if (arr.indexOf(elem) > -1) {
+                // IF arr's index is greater than -1
                 arr.splice(arr.indexOf(elem), 1);
+                // SPLICE elem out of arr
             }
         }
     }
