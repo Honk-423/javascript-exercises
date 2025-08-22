14d53e57261e88b7033819d89986d899a3f2d8b5
Talia <139439068+Honk-423@users.noreply.github.com>
2025-08-22 16:23:24 -0500

    docs: write notes as a guide

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,12 @@
 const sumAll = function() {
-
 };
+
+// There will be two numbers as parameters. I'll have to loop from the
+// smaller number to the larger one to generate all of the numbers in 
+// between, and somehow I'll have to add them all to the same array.
+// Then I'll probably have to reduce the array containing all of them.
+// It's also important that I set reduce's intial value to 0, if I do
+// use reduce.
 
 // Do not edit below this line
 module.exports = sumAll;
