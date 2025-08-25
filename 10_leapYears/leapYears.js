af2e682295294a95162c7162c0ae09e5ab7177e6
Talia <139439068+Honk-423@users.noreply.github.com>
2025-08-25 16:30:52 -0500

    docs: write pseudocode for leapYears

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,10 @@
 const leapYears = function() {
-
+// INIT param year
+// IF year divided by 4 equals true AND divided by 100 equals false 
+    // RETURN year
+// IF year divided by 4 equals true AND divided by 100 equals true, 
+// AND divided by 400 equals true
+    // RETURN year
 };
 
 // Do not edit below this line
