5a9af93f69bbc09c34bbe5a59654984a7094ae61
Talia <139439068+Honk-423@users.noreply.github.com>
2025-08-26 15:49:00 -0500

    fix: convert all equality operaters to strict
    Prevents possible bugs.

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,13 +1,13 @@
 const leapYears = function(year) {
 // INIT param year
     
-if (year % 4 == 0 && year % 100 != 0) {
+if (year % 4 === 0 && year % 100 !== 0) {
     // IF remainder of year divided by 4 equals 0 AND remainder of year 
     // divided by 100 does not equal 0
     return true;
         // RETURN true
 }
-if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0) {
+if (year % 4 === 0 && year % 100 === 0 && year % 400 === 0) {
     // IF remainder of year divided by 4 equals 0 AND remainder of year 
     // divided by 100 equal 0, AND remainder of year divided by 400 
     // equals 0
