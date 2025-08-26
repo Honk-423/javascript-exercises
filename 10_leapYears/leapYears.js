9c64444efe2e85350c8e54c1f4942275cfc49baf
Talia <139439068+Honk-423@users.noreply.github.com>
2025-08-26 15:24:09 -0500

    feat: make leapYears detect non-century leap years

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,11 +1,22 @@
-const leapYears = function() {
+const leapYears = function(year) {
 // INIT param year
-// IF year divided by 4 equals true AND divided by 100 equals false 
-    // RETURN year
-// IF year divided by 4 equals true AND divided by 100 equals true, 
-// AND divided by 400 equals true
-    // RETURN year
+    
+if (year % 4 == 0 && year % 100 != 0) {
+    // IF year divided by 4 equals true AND divided by 100 equals false 
+    return true;
+        // RETURN year
+}
+if (year % 4 == 0 && year % 100 != 0 && year % 400 === true) {
+    // IF year divided by 4 equals true AND divided by 100 equals true, 
+    // AND divided by 400 equals true
+    return true;
+        // RETURN year
+} else {
+    return false;
+}
 };
+
+leapYears(1996);
 
 // Do not edit below this line
 module.exports = leapYears;
