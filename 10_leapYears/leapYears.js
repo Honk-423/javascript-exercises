3b888f7bfd3bbf93b77c90f357aa70288c765411
Talia <139439068+Honk-423@users.noreply.github.com>
2025-08-26 15:29:19 -0500

    docs: update and add comments
    Some comments were made before implementing the changes required to
    make the code function as intended, so they were outdated.
    There were also a few lines that didn't have any comments, so I added
    them.

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -2,17 +2,21 @@ const leapYears = function(year) {
 // INIT param year
     
 if (year % 4 == 0 && year % 100 != 0) {
-    // IF year divided by 4 equals true AND divided by 100 equals false 
+    // IF remainder of year divided by 4 equals 0 AND remainder of year 
+    // divided by 100 does not equal 0
     return true;
-        // RETURN year
+        // RETURN true
 }
 if (year % 4 == 0 && year % 100 != 0 && year % 400 === true) {
-    // IF year divided by 4 equals true AND divided by 100 equals true, 
-    // AND divided by 400 equals true
+    // IF remainder of year divided by 4 equals 0 AND remainder of year 
+    // divided by 100 equal 0, AND remainder of year divided by 400 
+    // equals 0
     return true;
-        // RETURN year
+        // RETURN true
 } else {
+    // ELSE
     return false;
+    // RETURN false
 }
 };
 
