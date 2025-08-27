285a4fff5b4801edd63d933db46d85876462727c
Talia <139439068+Honk-423@users.noreply.github.com>
2025-08-27 15:33:43 -0500

    docs: edit pseudocode to provide more clarity

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -2,16 +2,14 @@ const convertToCelsius = function(fahrenheit) {
   // INIT param fahrenheit
   const celcius = (fahrenheit - 32) / 1.8;
   // INIT const celsius with the value (fahrenheit - 32) / (1.8)
-  // ROUND celsius
   return +celcius.toFixed(1);
-  // RETURN celsius
+  // RETURN and ROUND celcius to the tenths' place
 };
 
 const convertToFahrenheit = function() {
   // INIT param celsius
   // INIT const fahrenheit with the value (celcius * 1.8) + 32
-  // ROUND fahrenheit
-  // RETURN fahrenheit
+  // RETURN and ROUND fahrenheit to the tenths' place
 };
 
 // Do not edit below this line
