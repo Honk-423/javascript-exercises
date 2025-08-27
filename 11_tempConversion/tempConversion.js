e082ace5435ff6e5e522bb40405d0f120a2f9143
Talia <139439068+Honk-423@users.noreply.github.com>
2025-08-27 15:08:11 -0500

    docs: add pseudocode to tempConversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
 const convertToCelsius = function() {
+  // INIT param fahrenheit
+  // INIT const celsius with the value (fahrenheit - 32) / (1.8)
+  // ROUND celsius
+  // RETURN celsius
 };
 
 const convertToFahrenheit = function() {
+  // INIT param celsius
+  // INIT const fahrenheit with the value (celcius * 1.8) + 32
+  // ROUND fahrenheit
+  // RETURN fahrenheit
 };
 
 // Do not edit below this line
