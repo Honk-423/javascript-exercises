800a9dfbb469ce1b342afd9d1cf3760204438c0d
Talia <139439068+Honk-423@users.noreply.github.com>
2025-10-03 16:50:34 -0500

    Write pseudocode
    Provides a starting point and to-do list.

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -2,5 +2,15 @@ const palindromes = function () {
 
 };
 
+/* 
+*   SET parameter to str
+*   REMOVE non-letters and spaces from str
+*   MAKE str case-insensitive
+*   INITIALIZE arr then CONVERT str to an arr
+*   INITIALIZE reversedArr with the value being arr.reverse
+*   IF arr equals reversedArr return true
+*   ELSE return false
+*/
+
 // Do not edit below this line
 module.exports = palindromes;
