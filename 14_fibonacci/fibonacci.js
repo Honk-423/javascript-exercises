b2cd07bb7bf47c7a589fd9db6a435eecd57e6305
Talia <139439068+Honk-423@users.noreply.github.com>
2025-10-09 17:18:23 -0500

    Initialize vars curr and next in func scope
    These variables are important for adding the current and next number
    together, which should generate the correct fibonacci number. They
    were initialized in global scope so that when I create the variable
    sum I can reference curr and next to add them together.

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,8 +1,12 @@
 const fibonacci = function(indexNum) {
     let arr = [];
 
+    let curr;
+    let next;
+
     for (let i = 0; arr.length < indexNum; i++) {
-  
+        curr = arr[i];
+        next = arr[i] + 1;
     };
 
     console.log(arr);
