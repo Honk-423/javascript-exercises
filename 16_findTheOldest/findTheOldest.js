4815402fe63355b9e732c10cc66e7d243c2046db
Talia <139439068+Honk-423@users.noreply.github.com>
2025-10-10 13:59:11 -0500

    Write pseudocode

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -2,5 +2,21 @@ const findTheOldest = function() {
 
 };
 
+/* 
+*   INITIALIZE parameter arrOfObjs so user knows what type of argument
+*   is expected.
+
+*   IF person's year of death is undefined
+*       USE date to find out how old they would be assuming they were 
+*       still alive today.
+*   ELSE
+*       SUBTRACT each person's year of death by their year of birth to 
+*       find out how old they were.
+
+*   COMPARE ages to find the oldest person.
+
+*   RETURN oldest person's entire object.
+*/
+
 // Do not edit below this line
 module.exports = findTheOldest;
